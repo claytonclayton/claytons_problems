8eb345d6aceeecd4f6a0a9e104f488b89b8f9199
clayton <ashleyclayton@student.unsw.edu.au>
2024-09-11 22:18:17 +1000

    finidhed implementing first draft of check.sh. incorrectly reports WA on certain tests

--- check.sh
+++ check.sh
@@ -1,36 +1,70 @@
 #!/bin/bash
 
+# Run from task directory after running bash ../gen-output.sh
+# Usage example: bash ../check.sh
+
+# Description: Tests all solutions in solutions/accepted against
+# expected output. Green for AC, red for WA, yellow for TLE. 
+
 time_limit=1
 
-g++ ../solutions/accepted/clayton.cpp -o clayton
-g++ ../solutions/accepted/clayton-graph.cpp -o clayton-graph
-frank=../solutions/frank.py
+i=1
+names=()
+ex=()
+for sol in solutions/accepted/*; do
+    run_solution=./solution
+
+    if [[ $sol == *.cpp ]]; then
+        g++ -O2 -std=c++17 $sol -o $i
+        ex+=(./$i)
+    elif [[ $sol == *.c ]]; then
+        gcc -O2 -lm $sol -o $i
+        ex+=(./$i)
+    elif [[ $sol == *.py ]]; then
+        ex+=("python3 $sol")
+    else
+        echo "unsupported file extension"
+        continue
+    fi
+
+    short=${sol#solutions/accepted/}
+    short=${short%.*}
+    names+=($short)
+    ((i++))
+done
 
 check() {
     x=$1
-    file=$2
-    expected=$3
-    short=${x#"../solutions/"}
-    actual=$(timeout $time_limit ./$x < $file)
+    name=$2
+    file=$3
+    expected=$4
+
+    actual=$(timeout $time_limit $x < $file)
     exit_code=$?
     if [ $exit_code -ne 0 ]; then
-        printf "\033[1;33m$short\033[0m "
-    elif diff $expected <(echo $actual) > /dev/null; then
-        printf "\033[1;32m$short\033[0m "
+        printf "\033[1;33m$name\033[0m "
+    elif diff -wq $expected <(echo $actual) > /dev/null; then
+        printf "\033[1;32m$name\033[0m "
     else
-        printf "\033[1;31m$short\033[0m "
+        printf "\033[1;31m$name\033[0m "
     fi
+
 }
 
-for file in $(find ../data -name "*.in" | sort); do
+for file in $(find data -name "*.in" | sort); do
     expected=${file%in}out
-    short=${file#"../data/"}
+    short=${file#"data/"}
     short=${short%.in}
     echo -n $short ""
-    check clayton-graph $file $expected
-    check clayton $file $expected
-    check $frank $file $expected
+
+    for i in ${!names[@]}; do
+        check "${ex[$i]}" "${names[$i]}" $file $expected
+    done
     echo ""
 done
 
-rm clayton clayton-graph
+for i in "${ex[@]}"; do
+    if [[ $i =~ ^\./ ]]; then
+        rm ${i#./}
+    fi
+done
