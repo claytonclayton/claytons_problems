690ac033bc72dc8055d5c190b84964707829da4d
clayton <ashleyclayton@student.unsw.edu.au>
2024-09-13 20:00:24 +1000

    added new bash generator to link with new random generator

--- hestia/gen/gen.sh
+++ hestia/gen/gen.sh
@@ -1,2 +1,15 @@
+# Run from task directory
+# Usage example: bash gen/gen.sh
 
-echo "before the introduction of testlib.h into cpmsoc's problem writing team we wrote generators fully in generator.cpp"
+mkdir -p data/random
+
+echo "Generating inputs"
+g++ -std=c++17 -O2 -o random gen/random.cpp
+
+./random 10 1 1 0 2 3 > data/random1/01.in
+
+rm random
+
+echo "Generating outputs"
+bash ../gen-output.sh solutions/accepted/solution.cpp
+
