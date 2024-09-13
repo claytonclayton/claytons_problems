07d797a714009863c5b761f762d1ec6142bc770b
clayton <ashleyclayton@student.unsw.edu.au>
2024-09-13 21:21:48 +1000

    removed old validator

--- hestia/assert_constraint.py
+++
@@ -1,8 +0,0 @@
-
-for i in range(3, 39):
-    with open("data/random/"+str(i)+".in", 'r') as f:
-        nums = [int(j) for j in f.read().split()[1:]]
-
-    for j in range(len(nums) - 1):
-        for k in range(j + 1, len(nums)):
-            assert nums[j] * nums[k] <= 10 ** 18, f'{i}, {j}, {k}'
